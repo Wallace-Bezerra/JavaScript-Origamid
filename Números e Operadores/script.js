de75b22780c77330e2c7c468388403e1ba65173c
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-08-14 10:31:32 -0300

    numeros-operadores

---
+++ Números e Operadores/script.js
@@ -0,0 +1,22 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var teste = "ola 2";
+console.log(teste / 2);
+console.log(+teste);
+
+// Somar a string '200' com o número 50 e retornar 250
+var string = '200';
+var resultado = +string + 50;
+console.log(resultado);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var number = 5
+console.log(++number);
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
---
+++ Variáveis/script.js
@@ -0,0 +1,35 @@
+var meuNome = "Wallace";
+var idade = 20;
+var comidaFavorita;
+comidaFavorita = "lasanha";
+
+var cidade,
+  estado,
+  pais,
+  endereco,
+  cep;
+
+var teste = null;
+console.log(typeof teste);
+
+// Declare uma variável contendo uma string
+var nome = "wallace";
+
+// Declare uma variável contendo um número dentro de uma string
+var numero = "2022";
+// Declare uma variável com a sua idade
+var idade = 20;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var primeiroNome = "wallace";
+var sobrenome = "bezerra";
+var nomeCompleto = `Meu nome completo: ${primeiroNome} ${sobrenome}`;
+console.log(nomeCompleto);
+// Coloque a seguinte frase em uma variável: It's time
+var frase1 = 'It\'s time';
+//ou
+var frase2 = "It's time"
+console.log(frase1, frase2);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof idade);
