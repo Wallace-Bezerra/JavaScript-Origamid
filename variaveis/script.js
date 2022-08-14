e6ffe73751a4265add693e05d6d1c95024b526a3
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-08-13 21:59:21 -0300

    first commit

---
+++ variaveis/script.js
@@ -0,0 +1,17 @@
+var meuNome = "Wallace";
+var idade = 20;
+var comidaFavorita;
+comidaFavorita = "lasanha";
+
+var pessoas = [1, 2, 3];
+pessoas[0];
+
+
+var cidade,
+    estado,
+    pais,
+    endereco,
+    cep;
+
+var teste = null;
+console.log(typeof teste);
