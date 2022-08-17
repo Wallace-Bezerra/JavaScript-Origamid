a7bbba5bce38fd9cd918ebf8002a7aa7a2ccf0b1
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-08-17 20:35:44 -0300

    objetos-exercicios-feito

--- Objetos/script.js
+++ Objetos/script.js
@@ -13,3 +13,63 @@ var pessoa = {
 console.log(pessoa.estado);
 pessoa.teste2();
 pessoa.teste();
+///
+var objeto = {
+    nome: 'wallace',
+    idade: 20,
+    cidade: "São Paulo",
+}
+
+objeto.teste = "oi";
+
+objeto.nome;
+
+// EXERCICIOS OBJETOS
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var dados = {
+    nome: "wallace",
+    sobrenome: "bezerra da silva",
+    idade: 20,
+    cidade: "São Paulo",
+    teste() {
+        return "teste objeto";
+    }
+}
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+dados.mostrarNomeCompleto = () => {
+    return `${dados.nome} ${dados.sobrenome}`;
+}
+
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+}
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+var cachorro = {
+    raça: "labrador",
+    cor: "preto",
+    idade: 10,
+    late(homem) {
+        if (homem) {
+            console.log("AUAU");
+            console.log(typeof homem);
+        }
+        else {
+            console.log('Não late');
+        }
+    }
+
+
+}
+
