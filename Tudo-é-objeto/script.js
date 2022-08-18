53e802c28fddf7fe911b4681d11b4005cbd957de
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-08-17 21:36:27 -0300

    tudo-e-objeto

---
+++ Tudo-é-objeto/script.js
@@ -0,0 +1,28 @@
+// nomeie 3 propriedades ou métodos de strings
+var string = "olr";
+console.log(string.charAt(2));
+string.length;
+string.toUpperCase();
+string.includes("ol");
+// nomeie 5 propriedades ou métodos de elementos do DOM
+document.querySelector();
+addEventListener();
+document.getElementById();
+navigator.clipboard.writeText("testeJavascript");
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
